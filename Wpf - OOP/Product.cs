0ffcce2e521bc8d75329e75879e2eed24fb79fc7
Sebastia n <KamashiVentures@gmail.com>
2025-03-03 15:38:11 +0100

    WPF - OOP

---
+++ Wpf - OOP/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf___OOP
+{
+    //nazwe klasy zazwyczaj to rzeczownik w l.poj.
+    class Product
+    {
+        //string name - nazywamy polem. Bez podania żadnego modyfikatora, to domyślnie jest to
+        //zmienna prywatna. Oznacza to że dzięki temu będzie dostępne tylko wewnątrz tej klasy.
+        private string _name; //przed nazwą prywatnego pola, dajemy _.
+        public Product(string name)
+        {
+            _name = name;   
+        }
+    }
+}
